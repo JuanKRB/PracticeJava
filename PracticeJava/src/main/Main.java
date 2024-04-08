21588c481a7d59e47be2ecd45e7cdc6a0758b655
JuanKRB <juanca.rodriguez.bon@gmail.com>
2024-04-08 14:59:58 -0600

    Awt library and points

--- PracticeJava/src/main/Main.java
+++ PracticeJava/src/main/Main.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.awt.Point;
+import java.util.Date;
+
 public class Main {
 	
 	public static void main(String[] args) {
@@ -11,11 +14,24 @@ public class Main {
 		double d = 363636373.3773;
 		boolean isTrue = false;
 		
-		
-		
 		System.out.println(a);
 		System.out.println(u);
 		System.out.println(c);
+		
+		
+		//**********************
+		
+		String hi = "hello";
+		System.out.println(hi);
+		
+		Date now = new Date();
+		System.out.println(now);
+		
+		Point point1 = new Point(10, 20);
+		Point point2 = new Point(15, 45);
+		
+		System.out.println("x: " + point1.x + ", y: " + point1.y);
+		System.out.println("x: " + point2.x + ", y: " + point2.y);
 	}
 
 }
