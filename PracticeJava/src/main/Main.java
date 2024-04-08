29c4f01e27e01fd53cc96187ed4a359951feac2a
JuanKRB <juanca.rodriguez.bon@gmail.com>
2024-04-08 16:48:55 -0600

    New

--- PracticeJava/src/main/Main.java
+++ PracticeJava/src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import java.awt.Point;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
 	
@@ -32,6 +33,32 @@ public class Main {
 		
 		System.out.println("x: " + point1.x + ", y: " + point1.y);
 		System.out.println("x: " + point2.x + ", y: " + point2.y);
+		
+		point2.y = 48;
+		
+		System.out.println("x: " + point1.x + ", y: " + point1.y);
+		System.out.println("x: " + point2.x + ", y: " + point2.y);
+		
+		//**********************
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		String name = scanner.nextLine();
+		System.out.println("Enter your age: ");
+		int age = scanner.nextInt();
+		System.out.println("Enter your gender: ");
+		char gender = scanner.next().charAt(0);
+		System.out.println("Enter your contact number:");
+		double number = scanner.nextDouble();
+		
+		System.out.println("User details: ");
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);	
+		System.out.println("Gender: "+ gender);
+		System.out.println("Contact number: "+ number);
+		
+		
+		
 	}
 
 }
