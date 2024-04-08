f83601f2118819698bfdeda5d043ab8d1ba517a6
JuanKRB <juanca.rodriguez.bon@gmail.com>
2024-04-08 12:48:52 -0600

    Commit 2

---
+++ PracticeJava/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		byte a = 22;
+		long b = 20000993;
+		float c = 34.5f;
+		double d = 363636373.3773;
+		boolean isTrue = false;
+		
+		System.out.println(a);
+	}
+
+}
