f3688969170879dc9313d51e9ec647b06af224ad
JuanKRB <juanca.rodriguez.bon@gmail.com>
2024-04-23 10:56:22 -0600

    20

---
+++ PracticeJava/src/main/ArithmeticAssignmentOperators.java
@@ -0,0 +1,30 @@
+package main;
+
+public class ArithmeticAssignmentOperators {
+
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		int num2 = 2;
+		
+		num1 = num2;
+		System.out.println("num1 = num2: " + num1);
+		
+		num1 += num2;
+		System.out.println("num1 += num2: " + num1);
+		
+		num1 -= num2;
+		System.out.println("num1 -= num2: " + num1);
+
+		num1 *= num2;
+		System.out.println("num1 *= num2: " + num1);
+		
+		num1 /= num2;
+		System.out.println("num1 /= num2: " + num1);
+		
+		num1 %= num2;
+		System.out.println("num1 %= num2: " + num1);
+		
+	}
+
+}
---
+++ PracticeJava/src/main/RelationalOperators.java
@@ -0,0 +1,21 @@
+package main;
+
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		int num2 = 12;
+		int num3 = 3;
+		
+		System.out.println("num1 == num2: " + (num1 == num2));
+		System.out.println("num2 >= num1: " + (num2 >= num1));
+		System.out.println("num2 <= num1: " + (num2 <= num1));
+		System.out.println("num2 != num3: " + (num2 != num3));
+		System.out.println("num1 > num3: " + (num1 > num3));
+		System.out.println("num1 < num3: " + (num1 < num3));
+		
+		
+	}
+
+}
