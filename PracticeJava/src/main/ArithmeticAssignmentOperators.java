7487db2259238e5be4ebaa755ae71df472cfa41c
JuanKRB <juanca.rodriguez.bon@gmail.com>
2024-04-29 16:46:56 -0600

    2

--- PracticeJava/src/main/ArithmeticAssignmentOperators.java
+++ PracticeJava/src/main/ArithmeticAssignmentOperators.java
@@ -25,8 +25,6 @@ public class ArithmeticAssignmentOperators {
 		num1 %= num2;
 		System.out.println("num1 %= num2:  " + num1);
 		
-		num1 %= num2;
-		System.out.println("num1 %= num2:  " + num1);
 		
 	}
 
