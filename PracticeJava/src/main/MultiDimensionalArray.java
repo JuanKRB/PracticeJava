048b82fb17a95b73783e35924a371e03b98ee266
JuanKRB <juanca.rodriguez.bon@gmail.com>
2024-05-04 23:48:56 -0600

    1

---
+++ PracticeJava/src/main/MultiDimensionalArray.java
@@ -0,0 +1,20 @@
+package main;
+
+public class MultiDimensionalArray {
+
+	public static void main(String[] args) {
+
+		int marks[][] = {
+				{ 23, 34 },
+				{ 45,22 }, 
+				{ 89, 56 }, };
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 2; j++) {
+				System.out.println("Row: " + i + ", Column: " + j + ", " + marks[i][j]);
+			}
+		}
+
+	}
+
+}
