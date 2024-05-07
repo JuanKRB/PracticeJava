4f7d15e338d7d853bd9788dc5a4eba5a29c36013
JuanKRB <juanca.rodriguez.bon@gmail.com>
2024-05-07 14:46:15 -0600

    1

---
+++ PracticeJava/src/main/StringManipulation.java
@@ -0,0 +1,21 @@
+package main;
+
+public class StringManipulation {
+
+	public static void main(String[] args) {
+		
+		//Using 	String literal
+		String str1 = "Hello";
+		
+		
+		//Using 	String object
+		String str2 = new String("World");
+		
+		//Concatenating Both strings
+		String str3 = str1 + str2;
+				
+		
+
+	}
+
+}
