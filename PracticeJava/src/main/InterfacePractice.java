22c8424fd49b5a240fa9e43daba05102f7df45c1
JuanKRB <juanca.rodriguez.bon@gmail.com>
2024-06-27 10:46:43 -0600

    1

--- PracticeJava/src/main/InterfacePractice.java
+++ PracticeJava/src/main/InterfacePractice.java
@@ -57,7 +57,6 @@ public class InterfacePractice extends Father implements MyInterface, MyInterfac
 		
 		System.out.println("*************");
 		System.out.println("*************");
-		
 			
 		System.out.println(interfacePractice.Firstname);
 		System.out.println(interfacePractice.id);
